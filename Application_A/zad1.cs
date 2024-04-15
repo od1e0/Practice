907a480389c05e3d02e02e31110661c452326f7e
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-15 13:57:20 +0300

    Complete Task1

---
+++ Application_A/zad1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectre.Console;
+
+namespace Application_A
+{
+    internal class zad1
+    {
+        static void Main(string[] args)
+        {
+            var a = AnsiConsole.Ask<double>("a=");
+            var b = AnsiConsole.Ask<double>("b=");
+            var c = AnsiConsole.Ask<double>("c=");
+
+            AnsiConsole.MarkupLine($"({a.ToString("F4")}+({b.ToString("F4")}+{c.ToString("F4")})=({a.ToString("F4")}+{b.ToString("F4")}+{c.ToString("F4")})");
+            AnsiConsole.MarkupLine("[lime]Для продолжения нажмите любую кнопку...[/]");
+            Console.ReadKey();
+        }
+    }
+}
