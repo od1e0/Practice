ca10bb455cca45f6e8f19790cb99c26a2010317a
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-16 12:48:21 +0300

    Complete Task2

---
+++ Subject_1/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Spectre.Console;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = AnsiConsole.Ask<double>("Введите a= ");
+            var b = AnsiConsole.Ask<double>("Введите b= ");
+            var c = AnsiConsole.Ask<double>("Введите c= ");
+
+            bool isIsosceles = IsIsosceles(a, b, c);
+            AnsiConsole.MarkupLine($"Треугольник с длинами сторон {a}, {b}, {c} - " +
+                           $"{(isIsosceles ? "является" : "не является")} равнобедренным");
+           Console.ReadKey();
+        }
+
+        static bool IsIsosceles(double a, double b, double c)
+        {
+            return a == b || a == c || b == c;
+        }
+    }
+}
