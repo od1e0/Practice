fe306efe86bf4cacc34c9a0f618edb1299ce0a86
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-16 12:56:01 +0300

    Complete Task3

---
+++ Subject_1/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Spectre.Console;
+
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var k = AnsiConsole.Ask<int>("Введите номер карты: ");
+
+            var cardNames = new Dictionary<int, string>
+            {
+                {14, "Туз"}, {13, "Король"}, {12, "Дама"},
+                {11, "Валет"}, {10, "Десятка"}, {9, "Девятка"},
+                {8, "Восьмерка"}, {7, "Семерка"}, {6, "Шестерка"}
+            };
+            if (cardNames.ContainsKey(k))
+                Console.WriteLine($"Ваша карта - {cardNames[k]}");
+            else
+                Console.WriteLine("Номер карты не входит в допустимый диапазон");
+
+            Console.ReadKey();
+        }
+
+    }
+}
