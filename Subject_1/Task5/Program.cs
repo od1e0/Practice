9ca1a8d44bb2da3be4bb3f7913593e6b3ea23bfb
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-16 13:13:40 +0300

    Complete Task5

---
+++ Subject_1/Task5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Spectre.Console;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = AnsiConsole.Ask<int>("Введите A= ");
+            int B = AnsiConsole.Ask<int>("Введите B= ");
+
+            if (A < B)
+            {
+                int sum = 0;
+                for (int i = A; i <= B; i++)
+                {
+                    sum += i;
+                }
+                AnsiConsole.WriteLine($"Сумма всех целых чисел от {A} до {B} включительно равна {sum}");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[red]A > B[/]");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
