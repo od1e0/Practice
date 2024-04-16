cfc3c534b5be32c088b214309b9bd6a4d6bee8af
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-16 13:23:33 +0300

    Complete Task6

---
+++ Subject_1/Task6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Spectre.Console;
+
+namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = Math.PI / 8; 
+            double B = 2 / Math.PI;
+            int M = 15; 
+
+            double H = (B - A) / M;
+
+            var table = new Table().Border(TableBorder.Rounded);
+            table.AddColumn("x");
+            table.AddColumn("F(x)");
+
+            for (int i = 0; i <= M; i++)
+            {
+                double x = A + i * H;
+                double F = Math.Sin(1 / x);
+                table.AddRow(x.ToString(), F.ToString());
+            }
+
+            AnsiConsole.Render(table);
+
+            Console.ReadKey();
+        }
+    }
+}
