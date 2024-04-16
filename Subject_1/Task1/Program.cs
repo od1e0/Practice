ff0094f5ce61abeb6e3156c3052dd3e440bdc9b8
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-16 12:37:07 +0300

    Complete Task1

--- Subject_1/Task1/Program.cs
+++ Subject_1/Task1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Spectre.Console;
 
 namespace Task1
 {
@@ -10,6 +11,26 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            var x = AnsiConsole.Ask<double>("x= ");
+            AnsiConsole.Render(new FigletText($"y = {CalculateY(x)}"));
+
+            Console.ReadKey();
+        }
+
+        static double CalculateY(double x)
+        {
+            if (1 < x && x < 2)
+            {
+                return Math.Pow(x - 2, 2) + 6;
+            }
+            else if (x >= 2)
+            {
+                return Math.Log(x + 3 * Math.Sqrt(x));
+            }
+            else
+            {
+                throw new ArgumentException("x не входит в допустимый диапазон значений");
+            }
         }
     }
 }
