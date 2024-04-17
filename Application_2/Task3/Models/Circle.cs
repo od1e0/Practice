ec89ac1e120c05d9b668d5ac0ee61038adb40118
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-17 13:36:29 +0300

    Complete Task3

---
+++ Application_2/Task3/Models/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task3.Models
+{
+    public class Circle
+    {
+        private double _x, _y, _radius;
+
+        public double X { get => _x; set => _x = value; }
+        public double Y { get => _y; set => _y = value; }
+        public double Radius { get => _radius; set => _radius = value; }
+
+
+        public Circle(double x, double y, double radius)
+        {
+            _x = x;
+            _y = y;
+            _radius = radius;
+        }
+
+        public double Area()
+        {
+            return Math.PI * Math.Pow(_radius, 2);
+        }
+
+        public double Circumference()
+        {
+            return 2 * Math.PI * _radius;
+        }
+
+        public bool IsPointInside(double x, double y)
+        {
+            double distance = Math.Sqrt(Math.Pow(x - _x, 2) + Math.Pow(y - _y, 2));
+            return distance <= _radius;
+        }
+
+        public override string ToString()
+        {
+            return $"Круг с центром в ({_x}, {_y}) и радиусом {_radius}";
+        }
+    }
+}
---
+++ Application_2/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Task3.Models;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(4, 5, 4);
+            Console.WriteLine(circle.ToString());
+            Console.WriteLine($"Длина окружности: {circle.Circumference()}");
+            Console.WriteLine($"Площадь: {circle.Area()}");
+            Console.WriteLine(circle.IsPointInside(3,3) ? "Точка попадает в окружность" : "Точка не попадает в окружность");
+
+            Console.ReadKey();
+        }
+    }
+}
