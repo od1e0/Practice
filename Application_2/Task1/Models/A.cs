5f3e732c4e257572898a6c91665d20aefc976850
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-17 12:52:26 +0300

    Complete Task1

---
+++ Application_2/Task1/Models/A.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task1.Models
+{
+    internal class A
+    {
+        private int _a;
+        private int _b;
+
+        public A(int a, int b)
+        {
+            _a = a;
+            _b = b;
+        }
+
+        public double СalculatExpressionValues() => ( 3 * _b - 2 / Math.Pow(_a,2)) / 4;
+
+        public double CalculateCube() => Math.Pow(_a / _b, 3);
+    }
+}
---
+++ Application_2/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Task1.Models;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите а: ");
+            int numA = int.Parse(Console.ReadLine());
+            Console.Write("Введите b: ");
+            int numB = int.Parse(Console.ReadLine());
+
+            A a = new A(numA, numB);
+
+            Console.WriteLine($"вызов метода вычисления значения выражения: {a.СalculatExpressionValues()}");
+            Console.WriteLine($"вызов метода возведения в куб частного a и b: {a.CalculateCube()}");
+
+            Console.ReadKey();
+        }
+    }
+}
