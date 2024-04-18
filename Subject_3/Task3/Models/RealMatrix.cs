a496304d08ab005151b2ab7631e4223badbcefb5
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-18 09:24:19 +0300

    Complete Task3

---
+++ Subject_3/Task3/Models/RealMatrix.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace Task3.Models
+{
+    public class RealMatrix
+    {
+        private double[,] matrix;
+
+        public RealMatrix(int rows, int cols)
+        {
+            matrix = new double[rows, cols];
+        }
+
+        public double this[int i, int j]
+        {
+            get { return matrix[i, j]; }
+            set { matrix[i, j] = value; }
+        }
+
+        public void InvertNonZeroReplaceZero()
+        {
+            var flattened = matrix.Cast<double>().Select(x => x == 0 ? 1 : -x).ToArray();
+            Buffer.BlockCopy(flattened, 0, matrix, 0, flattened.Length * sizeof(double));
+        }
+
+        public static RealMatrix operator +(RealMatrix a, RealMatrix b)
+        {
+            if (a.matrix.GetLength(0) != b.matrix.GetLength(0) || a.matrix.GetLength(1) != b.matrix.GetLength(1))
+            {
+                throw new ArgumentException("Матрицы должны быть одного размера для сложения.");
+            }
+
+            var result = new RealMatrix(a.matrix.GetLength(0), a.matrix.GetLength(1));
+            var aFlattened = a.matrix.Cast<double>().ToArray();
+            var bFlattened = b.matrix.Cast<double>().ToArray();
+            var sum = aFlattened.Zip(bFlattened, (x, y) => x + y).ToArray();
+
+            Buffer.BlockCopy(sum, 0, result.matrix, 0, sum.Length * sizeof(double));
+
+            return result;
+        }
+
+        public static RealMatrix operator -(RealMatrix a, RealMatrix b)
+        {
+            if (a.matrix.GetLength(0) != b.matrix.GetLength(0) || a.matrix.GetLength(1) != b.matrix.GetLength(1))
+            {
+                throw new ArgumentException("Матрицы должны быть одного размера для вычитания.");
+            }
+
+            var result = new RealMatrix(a.matrix.GetLength(0), a.matrix.GetLength(1));
+            var aFlattened = a.matrix.Cast<double>().ToArray();
+            var bFlattened = b.matrix.Cast<double>().ToArray();
+            var diff = aFlattened.Zip(bFlattened, (x, y) => x - y).ToArray();
+
+            Buffer.BlockCopy(diff, 0, result.matrix, 0, diff.Length * sizeof(double));
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return string.Join("\n", Enumerable.Range(0, matrix.GetLength(0))
+                .Select(i => string.Join("\t", Enumerable.Range(0, matrix.GetLength(1))
+                .Select(j => matrix[i, j].ToString()))));
+        }
+    }
+}
---
+++ Subject_3/Task3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task3.Models;
+using static Spectre.Console.AnsiConsole;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            int rows = 3;
+            int cols = 3;
+
+            RealMatrix matrix = new RealMatrix(rows, cols);
+            RealMatrix matrix1 = new RealMatrix(rows, cols);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = rand.NextDouble() * 10;
+                    matrix1[i, j] = rand.NextDouble() * 10;
+                }
+            }
+
+            MarkupLine("[green]Матрица:[/]");
+            WriteLine(matrix.ToString());
+
+            matrix.InvertNonZeroReplaceZero();
+
+            MarkupLine("\n[red]Измененная:[/]");
+            WriteLine(matrix.ToString());
+
+            MarkupLine("\n[blue]Сложение матриц:[/]");
+            WriteLine((matrix + matrix1).ToString());
+
+            System.Console.ReadKey();
+        }
+    }
+}
