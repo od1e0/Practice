9eed6b85e717b86fbe2e4785ca7831a95c2c14c9
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-18 09:33:41 +0300

    Complete Task5

---
+++ Subject_3/Task5/Models/Train.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task5.Models
+{
+    public class Train
+    {
+        private string destination;
+        private string trainNumber;
+        private DateTime departureTime;
+
+        public Train()
+        {
+        }
+
+        public Train(string destination, string trainNumber, DateTime departureTime)
+        {
+            this.destination = destination;
+            this.trainNumber = trainNumber;
+            this.departureTime = departureTime;
+        }
+
+        public string Destination
+        {
+            get { return destination; }
+            set { destination = value; }
+        }
+
+        public string TrainNumber
+        {
+            get { return trainNumber; }
+            set { trainNumber = value; }
+        }
+
+        public DateTime DepartureTime
+        {
+            get { return departureTime; }
+            set { departureTime = value; }
+        }
+    }
+}
---
+++ Subject_3/Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Task5.Models;
+using static Spectre.Console.AnsiConsole;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Train train = new Train("Moscow", "123A", DateTime.Now);
+
+            MarkupLine($"Направление: [green]{train.Destination}[/]");
+            MarkupLine($"Номер поезда: [green]{train.TrainNumber}[/]");
+            MarkupLine($"Время отправления: [green]{train.DepartureTime}[/]");
+
+            System.Console.ReadKey();
+        }
+    }
+}
