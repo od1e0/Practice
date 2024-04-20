aac25e5c10da75cf5d7d2b6577dcd0fd4bf51371
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-20 11:37:36 +0300

    Complete Task1

---
+++ Subject_4/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static Spectre.Console.AnsiConsole;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                double x = Ask<double>("Введите значение x: ");
+
+                double y1 = Math.Pow(Math.Sin(x), 3);
+                WriteLine("y = sin^3(x) = " + y1);
+
+                if (x == 1)
+                {
+                    throw new DivideByZeroException();
+                }
+                double y2 = (x / (x - 1)) - 3 * x;
+                WriteLine("y = (x / (x-1)) - 3x = " + y2);
+            }
+            catch (FormatException)
+            {
+                WriteLine("Ошибка формата. Пожалуйста, введите число.");
+            }
+            catch (DivideByZeroException)
+            {
+                WriteLine("Ошибка деления на ноль. x не должен быть равен 1.");
+            }
+        }
+    }
+}
