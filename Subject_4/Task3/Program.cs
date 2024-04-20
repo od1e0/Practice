d64082de9638b63f741433ff9e08c6beb6413b92
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-20 11:55:48 +0300

    Complete Task3

---
+++ Subject_4/Task3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static Spectre.Console.AnsiConsole;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double A = Ask<double>("Введите значение A: ");
+                double B = Ask<double>("Введите значение B: ");
+                double C = Ask<double>("Введите значение C: ");
+                double D = Ask<double>("Введите значение D: ");
+
+                if (A < -100 || A > 100 || B < -100 || B > 100 || C < -100 || C > 100 || D < -100 || D > 100)
+                {
+                    throw new Exception("Введенное значение выходит за допустимый диапазон (-100, 100).");
+                }
+
+                Minmax(ref A, ref B);
+                Minmax(ref A, ref C);
+                Minmax(ref A, ref D);
+                Minmax(ref B, ref C);
+                Minmax(ref B, ref D);
+                Minmax(ref C, ref D);
+
+                WriteLine("Минимальное значение: " + A);
+                WriteLine("Максимальное значение: " + D);
+            }
+            catch (FormatException)
+            {
+                WriteLine("Ошибка формата. Пожалуйста, введите число.");
+            }
+            catch (Exception ex)
+            {
+                WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+
+        static void Minmax(ref double X, ref double Y)
+        {
+            if (X > Y)
+            {
+                double temp = X;
+                X = Y;
+                Y = temp;
+            }
+        }
+        
+    }
+}
