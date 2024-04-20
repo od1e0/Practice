bf3bb2b822218ea0996cf6e0b6de12922038626d
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-20 11:42:44 +0300

    Task2

---
+++ Subject_4/Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static Spectre.Console.AnsiConsole;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                double x = Ask<double>("Введите значение x: ");
+
+                double f;
+                if (-3 < x && x < -1)
+                {
+                    f = 2 * x;
+                }
+                else if (-1 < x && x < 3)
+                {
+                    if (x == 0)
+                    {
+                        throw new DivideByZeroException();
+                    }
+                    f = -2 / x;
+                }
+                else if (x >= 3)
+                {
+                    f = x;
+                }
+                else
+                {
+                    throw new Exception("x выходит за диапазон допустимых значений.");
+                }
+
+                WriteLine("f = " + f);
+            }
+            catch (FormatException)
+            {
+                WriteLine("Ошибка формата. Пожалуйста, введите число.");
+            }
+            catch (DivideByZeroException)
+            {
+                WriteLine("Ошибка деления на ноль. x не должен быть равен 0.");
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+        }
+    }
+}
