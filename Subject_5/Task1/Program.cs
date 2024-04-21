851e82c07c7324be0c42e9c9e92f48d61a18382c
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-21 23:23:53 +0300

    Complete Task1

---
+++ Subject_5/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static Spectre.Console.AnsiConsole;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Random rand = new Random();
+            int[] array = new int[10];
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(-100, 100); 
+
+                if (array[i] > 0)
+                {
+                    MarkupLine($"[green]{array[i]}[/]");
+                    count++;
+                }
+            }
+
+            MarkupLine($"Количество положительных элементов: [green]{count}[/]");
+
+            System.Console.ReadLine();
+        }
+
+    }
+}
