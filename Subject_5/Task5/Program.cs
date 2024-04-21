af45a34690dabc24a73f4ae2fa9ee02fdcb02e14
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-21 23:40:16 +0300

    Complete Task5

---
+++ Subject_5/Task5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static Spectre.Console.AnsiConsole;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Ask<int>("Введите значение n: ");
+            double result = CalculateFunction(n);
+
+            MarkupLine($"Результат: [green]{result}[/]");
+
+            System.Console.ReadLine();
+        }
+        static double CalculateFunction(int n)
+        {
+            return 1 / Factorial(n + 3);
+        }
+
+        static double Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+
+        
+    }
+}
