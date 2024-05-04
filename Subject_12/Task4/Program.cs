077235c19d72a124f2735fca7cc81e9b573bc976
od1e0 <vladimir.zdit11@gmail.com>
2024-05-04 11:12:06 +0300

    Complete Task4

---
+++ Subject_12/Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        public delegate int RandomDelegate();
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            RandomDelegate[] delegates = new RandomDelegate[3];
+            delegates[0] = () => random.Next(1, 100);
+            delegates[1] = () => random.Next(1, 100);
+            delegates[2] = () => random.Next(1, 100);
+
+            Func<RandomDelegate[], double> Average = (delArray) =>
+            {
+                double sum = 0;
+                foreach (var del in delArray)
+                {
+                    sum += del();
+                }
+                return sum / delArray.Length;
+            };
+
+            Console.WriteLine($"Среднее арифметическое: {Average(delegates)}");
+
+            Console.ReadLine();
+        }
+    }
+}
