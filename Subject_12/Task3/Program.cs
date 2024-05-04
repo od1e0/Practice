49f6b767626b412a8c9f234c7994a4ddc56eafed
od1e0 <vladimir.zdit11@gmail.com>
2024-05-04 11:08:58 +0300

    Complete Task3

---
+++ Subject_12/Task3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        public delegate string StringDelegate(string s);
+
+        static void Main(string[] args)
+        {
+            StringDelegate SD;
+
+            SD = ToUpper;
+            Console.WriteLine(SD("hello"));
+
+            SD = ToLower;
+            Console.WriteLine(SD("WORLD"));
+
+            SD = Reverse;
+            Console.WriteLine(SD("12345"));
+
+            Console.ReadLine();
+        }
+
+        public static string ToUpper(string s)
+        {
+            return s.ToUpper();
+        }
+
+        public static string ToLower(string s)
+        {
+            return s.ToLower();
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
