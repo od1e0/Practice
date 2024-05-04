426f7d4c8fe119186fdf1935673d3f7f0ecf9467
od1e0 <vladimir.zdit11@gmail.com>
2024-05-04 11:34:15 +0300

    Complete Task1

---
+++ Subject_13/Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Program
+    {
+        public delegate string ColorDelegate();
+
+        public static void Main()
+        {
+            try
+            {
+                ColorDelegate color1 = new ColorDelegate(ReturnRed);
+                ColorDelegate color2 = new ColorDelegate(ReturnGreen);
+                ColorDelegate color3 = new ColorDelegate(ReturnBlue);
+
+                Console.WriteLine(color1());
+                Console.WriteLine(color2());
+                Console.WriteLine(color3());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static string ReturnRed()
+        {
+            return "Красный";
+        }
+
+        public static string ReturnGreen()
+        {
+            return "Зеленый";
+        }
+
+        public static string ReturnBlue()
+        {
+            return "Синий";
+        }
+    }
+}
