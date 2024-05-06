ddf7e1e1c3a87cea1679d58eac553869d3e1cdc1
od1e0 <vladimir.zdit11@gmail.com>
2024-05-06 10:55:02 +0300

    Complete Task2

---
+++ Subject_14/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using static Spectre.Console.AnsiConsole;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Thread t1 = new Thread(SumNumbers);
+            t1.Name = "Thread 1";
+            t1.Start();
+
+            Thread t2 = new Thread(SumNumbers);
+            t2.Name = "Thread 2";
+            t2.Start();
+
+            System.Console.ReadLine();
+        }
+
+        static void SumNumbers()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                sum += i;
+            }
+
+            stopwatch.Stop();
+            MarkupLine($"[red]{Thread.CurrentThread.Name}[/] completed. Sum: [green]{sum}[/]. Time elapsed: [green]{stopwatch.ElapsedMilliseconds}[/] ms");
+        }
+    }
+}
