3234a4781e9ce98ddc8c2f368f78bb310fd6f081
od1e0 <vladimir.zdit11@gmail.com>
2024-05-06 12:24:57 +0300

    Complete Task5-6

---
+++ Subject_16/Task5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] originalNumbers = { 20, 10, 4, -1, -2, 0, 0, 0, -10, 41, 62 };
+            File.WriteAllLines("original.txt", originalNumbers.Select(n => n.ToString()));
+
+            int[] numbers = File.ReadAllLines("original.txt").Select(int.Parse).ToArray();
+
+            using (StreamWriter writer = new StreamWriter("result.txt"))
+            {
+                int sum = numbers[0];
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    if ((numbers[i] >= 0 && sum >= 0) || (numbers[i] <= 0 && sum <= 0))
+                    {
+                        sum += numbers[i];
+                    }
+                    else
+                    {
+                        writer.WriteLine(sum);
+                        sum = numbers[i];
+                    }
+                }
+                writer.WriteLine(sum);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Subject_16/Task6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Чтение строк из исходного файла");
+            string[] lines = File.ReadAllLines("input.txt");
+
+            int lengthThreshold = 150;
+
+            Console.WriteLine("Переписывание строк в output1.txt, добавляя номер строки и количество символов");
+            File.WriteAllLines("output1.txt", lines.Select((line, index) => $"{line} (Строка {index + 1}, символов: {line.Length})"));
+
+            Console.WriteLine("Переписывание строк в output2.txt, длина которых больше заданного числа");
+            File.WriteAllLines("output2.txt", lines.Where(line => line.Length > lengthThreshold));
+
+            Console.WriteLine("Переписывание строк в output3.txt, длина которых четная");
+            File.WriteAllLines("output3.txt", lines.Where(line => line.Length % 2 == 0));
+
+            Console.WriteLine("Переписывание строк в output4.txt, удаляя символы на четных местах");
+            File.WriteAllLines("output4.txt", lines.Select(line => new string(line.Where((c, i) => i % 2 != 0).ToArray())));
+
+            Console.ReadLine();
+        }
+    }
+}
