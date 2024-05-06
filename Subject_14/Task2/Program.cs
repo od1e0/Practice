6a2765e4d898fd6e8ae581ca2f287f80baa331fa
od1e0 <vladimir.zdit11@gmail.com>
2024-05-06 11:07:48 +0300

    Complete Task3

--- Subject_14/Task2/Program.cs
+++ Subject_14/Task2/Program.cs
@@ -3,36 +3,36 @@ using System.Diagnostics;
 using System.Threading;
 using static Spectre.Console.AnsiConsole;
 
-namespace Task2
-{
-    class Program
+    namespace Task2
     {
-        static void Main()
+        class Program
         {
-            Thread t1 = new Thread(SumNumbers);
-            t1.Name = "Thread 1";
-            t1.Start();
-
-            Thread t2 = new Thread(SumNumbers);
-            t2.Name = "Thread 2";
-            t2.Start();
-
-            System.Console.ReadLine();
-        }
-
-        static void SumNumbers()
-        {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            int sum = 0;
-            for (int i = 1; i <= 10; i++)
+            static void Main()
             {
-                sum += i;
+                Thread t1 = new Thread(SumNumbers);
+                t1.Name = "Thread 1";
+                t1.Start();
+
+                Thread t2 = new Thread(SumNumbers);
+                t2.Name = "Thread 2";
+                t2.Start();
+
+                System.Console.ReadLine();
             }
 
-            stopwatch.Stop();
-            MarkupLine($"[red]{Thread.CurrentThread.Name}[/] completed. Sum: [green]{sum}[/]. Time elapsed: [green]{stopwatch.ElapsedMilliseconds}[/] ms");
+            static void SumNumbers()
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                int sum = 0;
+                for (int i = 1; i <= 10; i++)
+                {
+                    sum += i;
+                }
+
+                stopwatch.Stop();
+                MarkupLine($"[red]{Thread.CurrentThread.Name}[/] completed. Sum: [green]{sum}[/]. Time elapsed: [green]{stopwatch.ElapsedMilliseconds}[/] ms");
+            }
         }
     }
-}
---
+++ Subject_14/Task3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading;
+using System;
+using static Spectre.Console.AnsiConsole;
+
+namespace Task3
+{
+    class Program
+    {
+        static object lockObject = new object();
+
+        static void Main()
+        {
+            int A = Ask<int>("Введите значение A: ");
+
+            int N = Ask<int>("Введите значение N: ");
+
+            Thread t1 = new Thread(() => SumMethod(A, N));
+            t1.Start();
+
+            Thread t2 = new Thread(() => SumMethod(A, N));
+            t2.Start();
+
+            Thread t3 = new Thread(() => MultiplyMethod(A, N));
+            t3.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            System.Console.ReadLine();
+        }
+
+        static void SumMethod(int A, int N)
+        {
+            int sum = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                sum += A + i;
+            }
+            MarkupLine($"Сумма чисел от A до A+N в [red]{Thread.CurrentThread.ManagedThreadId}[/]: [green]{sum}[/]");
+        }
+
+        static void MultiplyMethod(int A, int N)
+        {
+            lock (lockObject)
+            {
+                int product = 1;
+                for (int i = 1; i <= N; i++)
+                {
+                    product *= A * i;
+                }
+                MarkupLine($"Произведение чисел от A до A*N в [red]{Thread.CurrentThread.ManagedThreadId}[/]: [green]{product}[/]");
+            }
+        }
+    }
+}
