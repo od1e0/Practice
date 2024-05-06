97379ca65fa9073cf97c83fb773454d22b289dfa
od1e0 <vladimir.zdit11@gmail.com>
2024-05-06 11:26:15 +0300

    Complete Task1

---
+++ Subject_14/Task4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace Task4
+{
+    class Program
+    {
+        static int[] numbers;
+        static int sum = 0;
+        static object lockObject = new object();
+
+        static void Main()
+        {
+            numbers = Enumerable.Range(0, 100).ToArray();
+
+            Thread t1 = new Thread(SumEvenNumbers);
+            Thread t2 = new Thread(SumEvenNumbers);
+
+            t1.Start(0);  
+            t2.Start(1);  
+
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine($"Сумма четных чисел: {sum}");
+
+            System.Console.ReadLine();
+        }
+
+        static void SumEvenNumbers(object start)
+        {
+            int startIndex = (int)start;
+            for (int i = startIndex; i < numbers.Length; i += 2)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    lock (lockObject)
+                    {
+                        sum += numbers[i];
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Subject_15/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
