a956411f5bcd1eebd54a86c276680ccc2a21f6aa
od1e0 <vladimir.zdit11@gmail.com>
2024-05-06 11:34:35 +0300

    Complete Task2

---
+++ Subject_15/Task2/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    public class MyDictionary<TKey, TValue>
+    {
+        private List<TKey> keys;
+        private List<TValue> values;
+
+        public MyDictionary()
+        {
+            keys = new List<TKey>();
+            values = new List<TValue>();
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                int index = keys.IndexOf(key);
+                if (index == -1)
+                {
+                    throw new KeyNotFoundException();
+                }
+                return values[index];
+            }
+        }
+
+        public int Count
+        {
+            get { return keys.Count; }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (keys.Contains(key))
+            {
+                throw new ArgumentException("An element with the same key already exists in the MyDictionary");
+            }
+            keys.Add(key);
+            values.Add(value);
+        }
+    }
+}
---
+++ Subject_15/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+
+            myDictionary.Add("one", 1);
+            myDictionary.Add("two", 2);
+            myDictionary.Add("three", 3);
+
+            Console.WriteLine($"Значение для 'one': {myDictionary["one"]}");
+            Console.WriteLine($"Значение для 'two': {myDictionary["two"]}");
+            Console.WriteLine($"Значение для 'three': {myDictionary["three"]}");
+
+            Console.WriteLine($"Общее количество пар элементов: {myDictionary.Count}");
+
+            Console.ReadLine();
+        }
+    }
+}
