733ad3b1bb7744eaef1e52f785f8597e2fc7ae42
od1e0 <vladimir.zdit11@gmail.com>
2024-05-10 11:46:17 +0300

    Complete Task1

---
+++ Subject_18/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abc#d##c";
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c != '#')
+                {
+                    stack.Push(c);
+                }
+                else if (stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+            }
+            char[] arr = stack.ToArray();
+            Array.Reverse(arr);
+            Console.WriteLine(new string(arr));
+
+            Console.ReadLine();
+        }
+    }
+}
