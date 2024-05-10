6d02a9e5768faa560eb86d305771c94692776dc6
od1e0 <vladimir.zdit11@gmail.com>
2024-05-10 12:49:33 +0300

    Complete Task3

---
+++ Subject_18/Task3/Journey.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Journey
+    {
+        public string Destination { get; set; }
+        public DateTime? StartDate { get; set; } 
+        public DateTime? EndDate { get; set; } 
+
+        public Journey(string destination, DateTime? startDate = null, DateTime? endDate = null)
+        {
+            Destination = destination;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public Journey Clone()
+        {
+            return new Journey(Destination, StartDate, EndDate);
+        }
+    }
+}
---
+++ Subject_18/Task3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Journey> journeyList = new List<Journey>();
+            Dictionary<string, Journey> journeyDict = new Dictionary<string, Journey>();
+
+            Journey journey1 = new Journey("Paris", DateTime.Now);
+            journeyList.Add(journey1);
+            journeyDict.Add(journey1.Destination, journey1);
+
+            Journey journey2 = journey1.Clone();
+            journey2.Destination = "London";
+            journeyList.Add(journey2);
+            journeyDict.Add(journey2.Destination, journey2);
+
+            foreach (var journey in journeyList)
+            {
+                Console.WriteLine($"Направлнение: {journey.Destination}, StartDate: {journey.StartDate}, EndDate: {journey.EndDate}");
+            }
+
+            journeyList.Remove(journey1);
+            journeyDict.Remove(journey1.Destination);
+
+            foreach (var journey in journeyList)
+            {
+                Console.WriteLine($"Направлнение: {journey.Destination}, StartDate: {journey.StartDate}, EndDate: {journey.EndDate}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
