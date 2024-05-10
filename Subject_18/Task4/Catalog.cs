c3fcc5819f5d97e726b003f1d1c37f0630c9f41d
od1e0 <vladimir.zdit11@gmail.com>
2024-05-10 12:55:36 +0300

    Complete Task4

---
+++ Subject_18/Task4/Catalog.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Catalog
+    {
+        private Hashtable albums;
+
+        public Catalog()
+        {
+            albums = new Hashtable();
+        }
+
+        public void AddAlbum(string albumName, List<string> songs)
+        {
+            albums[albumName] = songs;
+        }
+
+        public void RemoveAlbum(string albumName)
+        {
+            albums.Remove(albumName);
+        }
+
+        public void AddSong(string albumName, string song)
+        {
+            if (albums.ContainsKey(albumName))
+            {
+                ((List<string>)albums[albumName]).Add(song);
+            }
+        }
+
+        public void RemoveSong(string albumName, string song)
+        {
+            if (albums.ContainsKey(albumName))
+            {
+                ((List<string>)albums[albumName]).Remove(song);
+            }
+        }
+
+        public void PrintCatalog()
+        {
+            foreach (DictionaryEntry album in albums)
+            {
+                Console.WriteLine($"Album: {album.Key}");
+                foreach (string song in (List<string>)album.Value)
+                {
+                    Console.WriteLine($"  Song: {song}");
+                }
+            }
+        }
+    }
+}
---
+++ Subject_18/Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Catalog catalog = new Catalog();
+
+
+            catalog.AddAlbum("Album1", new List<string> { "Song1", "Song2" });
+
+            catalog.AddAlbum("Album2", new List<string> { "Song5", "Song6" });
+
+            catalog.AddSong("Album1", "Song3");
+
+            catalog.PrintCatalog();
+
+            catalog.RemoveSong("Album1", "Song2");
+
+            catalog.PrintCatalog();
+
+            catalog.RemoveAlbum("Album1");
+
+            catalog.PrintCatalog();
+
+            Console.ReadLine();
+        }
+    }
+}
