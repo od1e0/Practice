38fcbd26d559e3783d80f8933c7ea04e2a3245e9
od1e0 <vladimir.zdit11@gmail.com>
2024-05-10 12:02:04 +0300

    Complete Task2

---
+++ Subject_18/Task2/MyTask.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class MyTask
+    {
+        private Queue<char> nonDigitQueue = new Queue<char>();
+        private Queue<char> digitQueue = new Queue<char>();
+
+        public void ReadFile(string filePath)
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                int c;
+                while ((c = sr.Read()) != -1)
+                {
+                    FillQueues((char)c);
+                }
+            }
+        }
+
+        private void FillQueues(char character)
+        {
+            if (Char.IsDigit(character))
+            {
+                digitQueue.Enqueue(character);
+            }
+            else
+            {
+                nonDigitQueue.Enqueue(character);
+            }
+        }
+
+        public void PrintElements()
+        {
+            while (nonDigitQueue.Count > 0)
+            {
+                Console.Write(nonDigitQueue.Dequeue());
+            }
+
+            while (digitQueue.Count > 0)
+            {
+                Console.Write(digitQueue.Dequeue());
+            }
+        }
+    }
+}
---
+++ Subject_18/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyTask myTask = new MyTask();
+            myTask.ReadFile("C:\\Users\\Вова\\Desktop\\Practice\\Subject_18\\Task2\\input.txt");
+            myTask.PrintElements();
+
+            Console.ReadLine();
+        }
+    }
+}
