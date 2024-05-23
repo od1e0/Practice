f410ce80e776db0b356ee02234de83cc48fbb506
od1e0 <vladimir.zdit11@gmail.com>
2024-05-23 11:35:58 +0300

    Complete Task2-3

---
+++ Subject_23/Task2/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+        private Chart chart1;
+        private Button buttonCalc;
+        private TextBox textBoxXmin;
+        private TextBox textBoxXmax;
+        private TextBox textBoxStep;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeChart();
+            InitializeControls();
+        }
+
+        private void InitializeChart()
+        {
+            chart1 = new Chart();
+            chart1.Dock = DockStyle.Top;
+            chart1.Height = 400;
+
+            ChartArea chartArea = new ChartArea("MainArea");
+            chart1.ChartAreas.Add(chartArea);
+
+            Series series1 = new Series("c(x)");
+            series1.ChartType = SeriesChartType.Spline;
+            series1.BorderWidth = 2;
+            series1.Color = System.Drawing.Color.Blue;
+            chart1.Series.Add(series1);
+
+            this.Controls.Add(chart1);
+        }
+
+        private void InitializeControls()
+        {
+            // Создаем текстовые поля для ввода Xmin, Xmax и Step
+            textBoxXmin = new TextBox { Dock = DockStyle.Top, Text = "Введите Xmin" };
+            textBoxXmax = new TextBox { Dock = DockStyle.Top, Text = "Введите Xmax" };
+            textBoxStep = new TextBox { Dock = DockStyle.Top, Text = "Введите шаг" };
+
+            // Создаем кнопку
+            buttonCalc = new Button
+            {
+                Text = "Расчёт",
+                Dock = DockStyle.Bottom
+            };
+            buttonCalc.Click += new EventHandler(buttonCalc_Click);
+
+            // Добавляем элементы управления на форму
+            this.Controls.Add(textBoxStep);
+            this.Controls.Add(textBoxXmax);
+            this.Controls.Add(textBoxXmin);
+            this.Controls.Add(buttonCalc);
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Считываем с формы требуемые значения
+                double Xmin = double.Parse(textBoxXmin.Text);
+                double Xmax = double.Parse(textBoxXmax.Text);
+                double Step = double.Parse(textBoxStep.Text);
+
+                // Обновляем график для значений x в заданном диапазоне
+                UpdateChart(Xmin, Xmax, Step);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Пожалуйста, введите правильные числовые значения.");
+            }
+        }
+
+        private double CalculateFunction(double x, double y, double z)
+        {
+            return Math.Pow(2, Math.Pow(y, x)) + Math.Pow(Math.Pow(3, x), y) - (y * (Math.Atan(z) - Math.PI / 6)) / (Math.Abs(x) + 1 / (y * y + 1));
+        }
+
+        private void UpdateChart(double Xmin, double Xmax, double Step)
+        {
+            // Заданные значения y и z
+            double y = 0.827;
+            double z = 25.001;
+
+            // Количество точек
+            int count = (int)Math.Ceiling((Xmax - Xmin) / Step) + 1;
+
+            // Массивы значений X и C
+            double[] xValues = new double[count];
+            double[] cValues = new double[count];
+
+            // Вычисляем точки для графика функции
+            for (int i = 0; i < count; i++)
+            {
+                double x = Xmin + Step * i;
+                xValues[i] = x;
+                cValues[i] = CalculateFunction(x, y, z);
+            }
+
+            // Очищаем предыдущие точки графика
+            chart1.Series["c(x)"].Points.Clear();
+
+            // Добавляем вычисленные значения в график
+            chart1.Series["c(x)"].Points.DataBindXY(xValues, cValues);
+        }
+    }
+}
---
+++ Subject_23/Task3/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(Form1_Paint);
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            // Настройка кистей и перьев
+            Pen blackPen = new Pen(Color.Black, 2);
+            Brush brownBrush = new SolidBrush(Color.Brown);
+            Brush whiteBrush = new SolidBrush(Color.White);
+            Brush blackBrush = new SolidBrush(Color.Black);
+
+            g.FillEllipse(brownBrush, 100, 50, 200, 150);
+            g.DrawEllipse(blackPen, 100, 50, 200, 150);
+
+            g.FillEllipse(whiteBrush, 140, 100, 30, 30);
+            g.FillEllipse(whiteBrush, 230, 100, 30, 30);
+            g.FillEllipse(blackBrush, 150, 110, 10, 10);
+            g.FillEllipse(blackBrush, 240, 110, 10, 10);
+
+            g.FillEllipse(blackBrush, 185, 155, 30, 20);
+
+            g.FillEllipse(brownBrush, 70, 50, 60, 100);
+            g.FillEllipse(brownBrush, 270, 50, 60, 100);
+            g.DrawEllipse(blackPen, 70, 50, 60, 100);
+            g.DrawEllipse(blackPen, 270, 50, 60, 100);
+
+            g.FillEllipse(brownBrush, 70, 200, 260, 150);
+            g.DrawEllipse(blackPen, 70, 200, 260, 150);
+
+            g.FillEllipse(brownBrush, 90, 330, 40, 70);
+            g.FillEllipse(brownBrush, 270, 330, 40, 70);
+            g.DrawEllipse(blackPen, 90, 330, 40, 70);
+            g.DrawEllipse(blackPen, 270, 330, 40, 70);
+
+            g.DrawLine(blackPen, 324, 250, 400, 200);
+
+            g.DrawArc(blackPen, 150, 165, 100, 50, 0, -180);
+        }
+    }
+}
