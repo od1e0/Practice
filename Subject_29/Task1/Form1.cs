c91be58f969ed8001dfe02fbc8948fea0dce273b
od1e0 <vladimir.zdit11@gmail.com>
2024-05-23 13:30:53 +0300

    Complete Task1-2

---
+++ Subject_29/Task1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private int angle = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTimer();
+        }
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 1000; 
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            angle += 6;
+            angle %= 360; 
+
+            Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            int centerX = ClientSize.Width / 2;
+            int centerY = ClientSize.Height / 2;
+
+            int endX = (int)(centerX + Math.Sin(angle * Math.PI / 180) * 50);
+            int endY = (int)(centerY - Math.Cos(angle * Math.PI / 180) * 50);
+
+            e.Graphics.DrawLine(Pens.Red, centerX, centerY, endX, endY);
+        }
+    }
+}
---
+++ Subject_29/Task2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+        Bitmap banner;
+        Rectangle rectangle;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(Form1_Paint);
+
+            try
+            {
+                banner = new Bitmap("baner.png");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка загрузки файла баннера\n " + ex.ToString(), "Баннер");
+                this.Close();
+                return;
+            }
+
+            rectangle = new Rectangle(100, 100, banner.Width, banner.Height);
+
+            timer1.Interval = 50;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rectangle.X -= 1;
+
+            if (Math.Abs(rectangle.X) > rectangle.Width)
+                rectangle.X += rectangle.Width;
+
+            this.Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; 
+            g.DrawImage(banner, rectangle);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Y >= rectangle.Y && e.Y <= rectangle.Y + rectangle.Height)
+            {
+                timer1.Enabled = false;
+            }
+            else
+            {
+                timer1.Enabled = true;
+            }
+        }
+    }
+}
