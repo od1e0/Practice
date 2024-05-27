6f336ba8d1e0d3428a2d4075e39b5b7cb67627ef
od1e0 <vladimir.zdit11@gmail.com>
2024-05-27 23:26:23 +0300

    Complette Subject_32

---
+++ Subject_32/Task2/Data/TourDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Task2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Task2.Data
+{
+    public class TourDbContext : DbContext
+    {
+        public DbSet<Tour> Tours { get; set; }
+        public DbSet<Tourist> Tourists { get; set; }
+
+        public TourDbContext(DbContextOptions<TourDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Subject_32/Task2/DatabaseTuristDataSet.cs
@@ -0,0 +1,7 @@
+﻿namespace Task2
+{
+}
+
+namespace Task2
+{
+}
