a8e0df49154c19940b399ad0a689f4fc8b7406cc
od1e0 <vladimir.zdit11@gmail.com>
2024-05-27 22:31:29 +0300

    Complete Subject_30

---
+++ Subject_30/Task2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Task2.Services;
+
+namespace Task2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly TourManager tourManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            tourManager = new TourManager("Data Source=DESKTOP-N2LUKQT;Initial Catalog=DBTur_firm;Integrated Security=True;TrustServerCertificate=True");
+            RefreshToursDataGrid();
+        }
+
+        private void AddTour_Click(object sender, RoutedEventArgs e)
+        {
+            string tourName = TourNameTextBox.Text;
+            tourManager.AddTour(tourName);
+            RefreshToursDataGrid();
+        }
+
+        private void AddTourist_Click(object sender, RoutedEventArgs e)
+        {
+            string touristName = TouristNameTextBox.Text;
+            tourManager.AddTourist(touristName); 
+            RefreshToursDataGrid();
+        }
+        private void RefreshToursDataGrid()
+        {
+            ToursDataGrid.ItemsSource = tourManager.GetTours();
+        }
+    }
+}
---
+++ Subject_30/Task2/Services/TourManager.cs
@@ -0,0 +1,77 @@
+﻿using Task2.Data;
+using Task2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Task2.Services
+{
+    internal class TourManager
+    {
+        private readonly string connectionString;
+
+        public TourManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        private TourDbContext GetDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TourDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            return new TourDbContext(optionsBuilder.Options);
+        }
+
+        public List<Tour> GetTours()
+        {
+            using (var context = GetDbContext())
+            {
+                return context.Tours.ToList();
+            }
+        }
+
+        public void AddTour(string tourName)
+        {
+            using (var context = GetDbContext())
+            {
+                var tour = new Tour { TourName = tourName };
+                context.Tours.Add(tour);
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteTour(int tourId)
+        {
+            using (var context = GetDbContext())
+            {
+                var tour = context.Tours.FirstOrDefault(t => t.TourId == tourId);
+                if (tour != null)
+                {
+                    context.Tours.Remove(tour);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void AddTourist(string touristName)
+        {
+            using (var context = GetDbContext())
+            {
+                var tourist = new Tourist { Name = touristName};
+                context.Tourists.Add(tourist);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateTouristName(int touristId, string updatedName)
+        {
+            using (var context = GetDbContext())
+            {
+                var tourist = context.Tourists.FirstOrDefault(t => t.TouristId == touristId);
+                if (tourist != null)
+                {
+                    tourist.Name = updatedName;
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
