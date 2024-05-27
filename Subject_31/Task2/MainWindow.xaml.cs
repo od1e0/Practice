ef337b1e109bee4c3e7636c716b9c46981ceee06
od1e0 <vladimir.zdit11@gmail.com>
2024-05-27 22:44:11 +0300

    Complete Subject_31

---
+++ Subject_31/Task2/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Task2.Services;
+
+namespace Task2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly TourManager tourManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            tourManager = new TourManager("Data Source=DESKTOP-N2LUKQT;Initial Catalog=DBTur_firm;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        private void ShowAllTours_Click(object sender, RoutedEventArgs e)
+        {
+            DataGridResults.ItemsSource = tourManager.GetAllTours();
+        }
+
+        private void DeleteTour_Click(object sender, RoutedEventArgs e)
+        {
+            int tourIdToDelete = 1;
+            tourManager.DeleteTour(tourIdToDelete);
+            ShowAllTours_Click(sender, e);
+        }
+
+        private void AddTourist_Click(object sender, RoutedEventArgs e)
+        {
+            int tourId = 1; 
+            string touristName = "John Doe"; 
+            tourManager.AddTourist(tourId, touristName);
+            ShowAllTours_Click(sender, e);
+        }
+
+        private void UpdateTourist_Click(object sender, RoutedEventArgs e)
+        {
+            int touristIdToUpdate = 1;
+            string updatedName = "Updated Name";
+            tourManager.UpdateTourist(touristIdToUpdate, updatedName);
+            ShowAllTours_Click(sender, e);
+        }
+    }
+}
---
+++ Subject_31/Task2/Models/Tourist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Models
+{
+    public class Tourist
+    {
+        [Key]
+        public int TouristId { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("Tour")]
+        public int TourId { get; set; }
+        public virtual Tour Tour { get; set; }
+    }
+}
